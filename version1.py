8356aae3f3981015cffeeeb633b5de28c244ca8d
Artemio <artemiolucero@gmail.com>
2022-11-27 15:07:24 -0300

    Ready

--- version1.py
+++ version1.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import sqlite3 as sq3 #Va a necesitar traer esta libreria a nuestro programa
+import sqlite3 as sq3
 #from asyncio.windows_events import NULL # Nos permite trabajar con la BBDD y registros NULL
 from tkinter import messagebox
 
@@ -13,8 +13,8 @@ from tkinter import messagebox
 def conectar():
     global con
     global cur
-    con = sq3.connect("my_db.db") #creara la base de datos
-    cur = con.cursor() #Cadete virtual nos traer y lleva los datos de y hacia la db
+    con = sq3.connect("my_db.db") 
+    cur = con.cursor()
     messagebox.showinfo("STATUS","Conectado a la BBDD!")
     
     #SQL para crear la tabla escuela
@@ -36,23 +36,21 @@ def conectar():
     email varchar(45) NOT NULL,
     FOREIGN KEY (id_escuela) REFERENCES escuelas(id))'''
     
-    cur.execute(instruct1)#ejecutara la creacion de escuelas
-    cur.execute(instruct2)#ejecutara la creacion de alumnos
+    cur.execute(instruct1)
+    cur.execute(instruct2)
     
     #DATOS PARA LAS TABLAS
     lista1=[(1,'Normal 1','Quilmes','Buenos Aires',250),(2,'Gral. San Martín','San Salvador','Jujuy',100),(3,'Belgrano','Belgrano','Córdoba',150),(4,'EET Nro 2','Avellaneda','Buenos Aires',500),(5,'Esc. N° 2 Tomás Santa coloma','Capital Federal','Buenos Aires',250)]
     lista2=[(1,2,1000,'Ramón Mesa',8,1,'rmesa@mail.com'),(2,2,1002,'Tomás Smith',8,1,''),(4,1,101,'Juan Perez',10,3,''),(5,1,105,'Pedro González',9,3,''),(6,5,190,'Roberto Luis Sánchez',8,3,'robertoluissanchez@gmail.com'),(7,2,106,'Martín Bossio',8,3,''),(8,4,100,'Paula Remmi',3,1,'mail@mail.com'),(9,4,1234,'Pedro Gómez',6,2,'')]
 
-    #Estructura de control, que nos evitara errores
+    
     try:
-        #Por ejemplo esta se va a ejecutar 5 veces, una por cada escuela
         cur.executemany("INSERT INTO escuelas VALUES (?,?,?,?,?)",lista1)
-        #Por ejemplo esta se va a ejecutar 9 veces, una por cada alumno
         cur.executemany("INSERT INTO alumnos VALUES (?,?,?,?,?,?,?)",lista2)
     except:
         pass
     
-    con.commit()#se encarga de que los cambios en la base de datos se guarden
+    con.commit()
 
 
 #DATOS PARA LAS TABLAS
@@ -64,9 +62,9 @@ lista2=[(1,2,1000,'Ramón Mesa',8,1,'rmesa@mail.com'),(2,2,1002,'Tomás Smith',8
 def salir():
     respuesta= messagebox.askquestion("CONFIRMACION","Está seguro que quiere salir?")
     if respuesta == "yes":
-        try:#intenta lo siguiente
+        try:
             con.close()
-        except:#si falla lo anterior hace lo siguiente
+        except:
             pass
         raiz.destroy()
         
@@ -96,6 +94,11 @@ def licencia():
 #Sobre la app
 def sobre_app():
     messagebox.showinfo("ACERCA DE...", "Creado por Marcia \n para Codo a Codo 4.0 - Big Data \n 2022")       
+
+
+#---------------
+#     CRUD
+#---------------
 
 #Leer
 def leer():
@@ -137,7 +140,21 @@ def crear():
 
 # Actualizar
 def actualizar():
-    pass
+    id_escuela=int(buscar_escuelas(TRUE)[0])
+    datos= id_escuela , alumnos.get(), calificacion.get(), email.get()
+    cur.execute("UPDATE alumnos SET id_escuela=?, nombre=?, nota=?, email=?", datos)
+    con.commit()
+    messagebox.showinfo("STATUS", "Registro actualizado.")
+    limpiar()
+
+#Eliminar
+def eliminar():
+    respuesta=messagebox.askquestion("CONFIRMACION","Esta seguro que quiere eliminar el registro?")
+    if respuesta == "yes":
+            nro_legajo=legajo.get()
+            cur.execute("DELETE FROM alumnos WHERE legajo="+ nro_legajo)
+            con.commit()
+            limpiar()
 
 # Listado de Alumnos
 def listado_alumnos():
@@ -189,20 +206,22 @@ def localizar_escuela(event):
 
     con.close()
 
+
+
 '''
 ========================
     INTERFAZ GRÁFICA
 ========================
 '''
-raiz = Tk() #Nombe de nuestra ventana principal, objeto de la clase Tk
+raiz = Tk()
 raiz.title("Python CRUD - Mamau")
 
-barramenu = Menu(raiz) #barramenu es un onjeto de la clase Menu que se ubicara en raiz
-raiz.config(menu = barramenu)#Con este renglon asignamos a el objeto raiz el objeto menu, pero esta vacio
+barramenu = Menu(raiz)
+raiz.config(menu = barramenu)
 
 #Menú BBDD
-bbddmenu = Menu (barramenu , tearoff = 0) # Lo ubicar en barramenu
-bbddmenu.add_command(label="Conectar", command=conectar) #agrega otra opcion al objeto barramenu
+bbddmenu = Menu (barramenu , tearoff = 0)
+bbddmenu.add_command(label="Conectar", command=conectar)
 bbddmenu.add_command(label="Listado de alumnos", command=listado_alumnos)
 bbddmenu.add_command(label="Salir", command=salir)
 barramenu.add_cascade(label="BBDD", menu= bbddmenu)
@@ -221,15 +240,13 @@ barramenu.add_cascade(label="Acerca de", menu=acercamenu)
 #FrameCampos
 framecampos = Frame(raiz)
 framecampos.config(background='CadetBlue')
-framecampos.pack() #si no le pasas argumentos, lo que hace pack es "a groso modo" ubica este objeto debajo del objeto anterior
+framecampos.pack()
 
-#Creamos las variables para guardar los datos de los entrys
-#no son clases de python, son de tkinter
 legajo = StringVar()
 alumnos = StringVar()
 email = StringVar()
 escuela = StringVar()
-calificacion = DoubleVar() # Numero con decimales
+calificacion = DoubleVar()
 localidad=StringVar()
 provincia =StringVar()
 
@@ -319,7 +336,7 @@ boton_actualizar = Button(framebotones,text="Actualizar", command=actualizar)
 boton_actualizar.grid(row=0, column=2,   pady=10 )
 
 #borrar
-boton_borrar = Button(framebotones,text="Borrar", command=limpiar)
+boton_borrar = Button(framebotones,text="Borrar", command=eliminar)
 boton_borrar.config(bg="red",fg="white")
 boton_borrar.grid(row=0, column=3, pady=10 )
 
@@ -332,4 +349,4 @@ copylabel= Label(framecopy, text="Copyright 2022 - Marcia")
 copylabel.config(background='LightYellow')
 copylabel.grid(row=0, column=0)
 
-raiz.mainloop() #No cierre la ventana cuando termine el programa, que la cierre el usuario
+raiz.mainloop()
